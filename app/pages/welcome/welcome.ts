7f5851e55e299726013195c163fd2415f4ea2ac7
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-07-10 23:14:27 +0200

    Updated auth.service and welcome page to handle token auth.

--- app/pages/welcome/welcome.ts
+++ app/pages/welcome/welcome.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, Toast } from 'ionic-angular';
 import { BarcodeScanner } from 'ionic-native';
 
+import { AuthService } from '../../services/auth.service';
+
+import { HomePage } from '../home/home';
+
 @Component({
-  templateUrl: 'build/pages/welcome/welcome.html',
+  templateUrl: 'build/pages/welcome/welcome.html'
 })
-export class WelcomePage {
+export class WelcomePage implements OnInit {
+  public barcode: String
 
-  constructor(private nav: NavController) {
+  constructor(private nav: NavController, private authService: AuthService) {
 
+  }
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.authenticateWithToken(token);
+    }
   }
 
   errorToast(message) {
@@ -21,11 +34,31 @@ export class WelcomePage {
   }
 
   turnScanner() {
-    BarcodeScanner.scan().then((barcodeData) => {
-      console.log(barcodeData)
-    }, (err) => {
-      this.nav.present(this.errorToast(err));
-    });
+    BarcodeScanner.scan()
+      .then(this.scanSuccess.bind(this))
+      .catch(this.scanError.bind(this));
+  }
+
+  scanSuccess({ text }) {
+    if (text) {
+      this.authenticateWithToken(text);
+    } else {
+      this.nav.present(this.errorToast('QR code is invalid'));
+    }
+  }
+
+  scanError(err) {
+    this.nav.present(this.errorToast(err));
+  }
+
+  authenticateWithToken(token) {
+    this.authService.authenticate(token)
+      .subscribe(() => {
+        localStorage.setItem('token', token);
+        this.nav.push(HomePage);
+      }, (err) => {
+        this.nav.present(this.errorToast(err))
+      });
   }
 
 }
--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import config from '../config';
 
 @Injectable()
-export class Auth {}
+export class AuthService {
+  constructor(public http: Http) {
+
+  }
+
+  authenticate(token) : Observable<Object> {
+    const body = JSON.stringify({ token });
+    const headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post(`${config.api}app/sign-in`, body, {
+      headers: headers
+    })
+    .map(res => res.json());
+  }
+}
