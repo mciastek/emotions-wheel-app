a0ecc1456d69dec8e20de235974ec4375a89db99
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-07-30 19:53:29 +0200

    Added languages watch to gulpfile. Added polish translations. Updated english translations.

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,8 @@
 export default {
   api: 'http://192.168.1.10:4000/api/v1/',
-  socket: 'ws://192.168.1.10:4000/socket'
+  socket: 'ws://192.168.1.10:4000/socket',
+
+  // When you add new translation file to "assets/i18n" path
+  // please update languages list by added language code
+  languages: ['en', 'pl']
 }
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task('watch', ['clean'], function(done){
     function(){
       gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
       gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+      gulpWatch('app/assets/**', function(){ gulp.start('copy-assets'); });
       buildBrowserify({ watch: true }).on('end', done);
     }
   );
