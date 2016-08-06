9c88cf30b6201fe0c3afc8d0a7463556cb0896ad
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2016-08-06 15:07:49 +0200

    Added photo-gallery container.

--- app/containers/index.ts
+++ app/containers/index.ts
@@ -1,15 +1,18 @@
 import { ExperimentBoard } from './experiment-board';
 import { PhotoPreview } from './photo-preview';
 import { BoardOverlay } from './board-overlay';
+import { PhotoGallery } from './photo-gallery';
 
 export {
   ExperimentBoard,
   PhotoPreview,
-  BoardOverlay
+  BoardOverlay,
+  PhotoGallery
 };
 
 export default [
   ExperimentBoard,
   PhotoPreview,
-  BoardOverlay
+  BoardOverlay,
+  PhotoGallery
 ];
---
+++ app/containers/photo-gallery/index.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'photo-gallery',
+  templateUrl: 'build/containers/photo-gallery/template.html',
+})
+export class PhotoGallery {
+
+}
